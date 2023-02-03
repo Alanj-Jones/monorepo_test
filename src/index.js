3548e7b3d384562d78b70362d0913eee3d55da42
Alan Jones <70040262+Alanj-Jones@users.noreply.github.com>
2023-02-03 16:52:13 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/index');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/api', router);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(3000, console.log(`Started application on port 3000`));
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+start(); 
