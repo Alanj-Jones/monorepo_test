707b458673d90bf5cbbb2d07ce0cf4f8a3a9368f
Alan Jones <alan.jones@revolt.digital>
2023-02-06 14:14:36 -0300

    Implemented basic CRUD functions

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,23 +1,34 @@
+const TasksService = require('../services/tasks.service');
 
 class TasksController {
-  getAllTasks = (req, res) => {
-    res.send('Sent all tasks');
+  getAllTasks = async (req, res) => {
+    const tasks = await TasksService.getAllTasks();
+    res.status(200).send({ data: tasks });
   };
   
-  getTaskById = (req, res) => {
-    res.send('Sent task with requested id');
+  getTaskById = async (req, res) => {
+    console.log(`res.param: ${JSON.stringify(req.params)}`);
+    const task = await TasksService.getTaskById(req.params.id);
+    res.status(200).json(task);
   }
   
-  createTask = (req, res) => {
-    res.send('created task');
+  createTask = async (req, res) => {
+    console.log(`req.body: ${JSON.stringify(req.body)}`);
+    const createdTask = await TasksService.createTask(req.body);
+    console.log(`task controller ${createdTask}`);
+    res.status(201).json(createdTask);
   }
   
-  updateTask = (req, res) => {
-    res.send('Updated task');
+  updateTask = async (req, res) => {
+    const updatedTask = await TasksService.updateTask(req.params.id, req.body);
+    console.log(JSON.stringify(updatedTask));
+    res.status(200).json(updatedTask);
   }
   
-  deleteTask = (req, res) => {
-    res.send('Deleted task');
+  deleteTask = async (req, res) => {
+    const response = await TasksService.deleteTask(req.params.id);
+    console.log(JSON.stringify(response));
+    res.status(204).send('Deleted task');
   }
   
 }
